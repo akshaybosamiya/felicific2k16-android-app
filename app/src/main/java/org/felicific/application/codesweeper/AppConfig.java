71695c38eb1a32a14a97fdd3a18fc9bed012c59d
Akshay Bosamiya <arbosamiya@gmail.com>
2017-01-01 17:56:55 +0530

    Initial Commit

---
+++ app/src/main/java/org/felicific/application/codesweeper/AppConfig.java
@@ -0,0 +1,23 @@
+package org.felicific.application.codesweeper;
+
+/**
+ * Created by Akshay Bosamiya on 15-10-2016.
+ */
+
+public class AppConfig {
+    /*public static String TECH_JSON = "http://192.168.1.103/feliappdata/tech.json";
+    public static String CULTURAL_JSON="http://192.168.1.103/feliappdata/cultural.json";
+    public static String CODEJAM_JSON="http://192.168.1.103/feliappdata/coding.json";
+    public static String ROBO_JSON="http://192.168.1.103/feliappdata/robotics.json";
+    public static String HUNT_JSON="http://192.168.1.103/feliappdata/huntronics.json";
+    public static String GAME_JSON="http://192.168.1.103/feliappdata/centralgaming.json";*/
+
+    public static String TECH_JSON = "http://feli16.orgfree.com/hello/tech.json";
+    public static String CULTURAL_JSON="http://feli16.orgfree.com/hello/cultural.json";
+    public static String CODEJAM_JSON="http://feli16.orgfree.com/hello/coding.json";
+    public static String ROBO_JSON="http://feli16.orgfree.com/hello/robotics.json";
+    public static String HUNT_JSON="http://feli16.orgfree.com/hello/huntronics.json";
+    public static String GAME_JSON="http://feli16.orgfree.com/hello/centralgaming.json";
+
+
+}
